c8dd38186b104e73b6c558d2c6356a0ad982861b
Roger Tubby <roger@sligowebworks.com>
2008-10-01 11:22:55 +0000

    checking in

---
+++ extensions/FCKeditor/FCKeditor.i18n.en.php
@@ -0,0 +1,22 @@
+<?php
+
+$allMessages = array(
+'textrichditor' => 'Rich Editor',
+'tog-riched_disable' => 'Rich Editor: disable globally',
+'tog-riched_disable_ns_main' => 'Rich Editor: disable within the main namespace',
+'tog-riched_disable_ns_talk' => 'Rich Editor: disable within the "{{ns:Talk}}" namespace',
+'tog-riched_disable_ns_user' => 'Rich Editor: disable within the "{{ns:User}}" namespace',
+'tog-riched_disable_ns_user_talk' => 'Rich Editor: disable within the "{{ns:User_talk}}" namespace',
+'tog-riched_disable_ns_project' => 'Rich Editor: disable within the "{{ns:Project}}" namespace',
+'tog-riched_disable_ns_project_talk' => 'Rich Editor: disable within the "{{ns:Project_talk}}" namespace',
+'tog-riched_disable_ns_image' => 'Rich Editor: disable within the "{{ns:Image}}" namespace',
+'tog-riched_disable_ns_image_talk' => 'Rich Editor: disable within the "{{ns:Image_talk}}" namespace',
+'tog-riched_disable_ns_mediawiki' => 'Rich Editor: disable within the "{{ns:MediaWiki}}" namespace',
+'tog-riched_disable_ns_mediawiki_talk' => 'Rich Editor: disable within the "{{ns:MediaWiki_talk}}" namespace',
+'tog-riched_disable_ns_template' => 'Rich Editor: disable within the "{{ns:Template}}" namespace',
+'tog-riched_disable_ns_template_talk' => 'Rich Editor: disable within the "{{ns:Template_talk}}" namespace',
+'tog-riched_disable_ns_help' => 'Rich Editor: disable within the "{{ns:Help}}" namespace',
+'tog-riched_disable_ns_help_talk' => 'Rich Editor: disable within the "{{ns:Help_talk}}" namespace',
+'tog-riched_disable_ns_category' => 'Rich Editor: disable within the "{{ns:Category}}" namespace',
+'tog-riched_disable_ns_category_talk' => 'Rich Editor: disable within the "{{ns:Category_talk}}" namespace',
+);
